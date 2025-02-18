c07b3a0218ad5bc1795cbd9210a29b80e35a67e2
Agent-cat <2300031042@kluniversity.in>
2025-02-18 09:29:33 +0530

    .

---
+++ Backend/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    id: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    branch: {
+        type: String,
+        required: true,
+    },
+    surabhiwing: {
+        type: String,
+        required: true,
+    },
+    phonenumber: {
+        type: Number,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
