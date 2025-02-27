cee57dd112a316758eb5deb42520707ddcf687c1
Agent-cat <2300031042@kluniversity.in>
2025-02-27 18:21:30 +0530

    .

--- Backend/routes/find.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const findUser = require('../controllers/find.controller');
-
-router.post('/user', findUser);
-
-module.exports = router;
